0173330083344210c8a82fcda9e765bf22bf5f10
Tudor Calin <calintudorsuciu@gmail.com>
2025-12-09 19:57:20 +0200

    Versiune v1.1 – noi funcționalități

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,21 +1,30 @@
 // src/pages/Home.js
 import React from "react";
-import { Link } from "react-router-dom";
+import "./App.css"; // păstrează stilurile de bază și adaugă noile clase
 
 export default function Home() {
   return (
-    <div className="uc-container">
-      <h1 className="uc-title">🚧 Under Construction 🚧</h1>
-      <div className="uc-animation">
-        <div className="uc-bar"></div>
-        <div className="uc-bar"></div>
-        <div className="uc-bar"></div>
-      </div>
-      <p className="uc-text">We are in testing phase. Stay tuned!</p>
+    <div className="uc-fullscreen">
+      <div className="uc-container">
+        <h1 className="uc-title">🚧 Under Construction 🚧</h1>
 
-      <Link to="/search">
-        <button className="btn-go-search">Caută anunțuri</button>
-      </Link>
+        <div className="uc-animation">
+          <div className="uc-bar"></div>
+          <div className="uc-bar"></div>
+          <div className="uc-bar"></div>
+        </div>
+
+        <p className="uc-text">
+          Will be back soon!
+        </p>
+
+        <p className="uc-text">
+          Contact:{" "}
+          <a href="mailto:calintudorsuciu@gmail.com" className="uc-mail">
+            calin@propr.ro
+          </a>
+        </p>
+      </div>
     </div>
   );
 }
